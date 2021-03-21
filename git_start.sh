35fc2905f49793fafe607c97e9e543c1ed13b326
GlitchYou <38540168+GlitchYou@users.noreply.github.com>
2021-03-21 14:43:08 -0300

    Update git_start.sh

--- git_start.sh
+++ git_start.sh
@@ -1,7 +1,10 @@
+#!/bin/bash
+
 if [ -z $1 ] && [ -z $2 ]; then
 	echo "Digite o nome e o seu email"
 	exit
 fi
+
 
 git init
 
@@ -12,11 +15,10 @@ read -p "Quer criar uma chave ssh? [S/n] "
 R=${REPLY^^}
 
 
-if [ -z "$R" ] || [ "$R" -eq "S" ]; then
+if [ -z "$R" ] || [ "$R" = "S" ]; then
 	ssh-keygen -t rsa -b 4096 -C "$1"
 	eval "$(ssh-agent -s)"
 
 	echo -e "Copie e cole o code ssh no GitHub\n\n"
 	cat ~/.ssh/id_rsa.pub
 fi
-
